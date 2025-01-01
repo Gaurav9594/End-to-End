b6dde15622df53ba643a25890e355616885ed7fb
Gaurav Vaishya <gauravvaishya143@gmail.com>
2025-01-01 18:53:31 +0530

    The last ShowDown

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -53,7 +53,6 @@ class ModelTrainer:
                 },
                 "Random Forest":{
                     # 'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
-                 
                     # 'max_features':['sqrt','log2',None],
                     'n_estimators': [8,16,32,64,128,256]
                 },
@@ -83,8 +82,7 @@ class ModelTrainer:
                 
             }
 
-            model_report:dict=evaluate_models(X_train=X_train,y_train=y_train,X_test=X_test,y_test=y_test,
-                                             models=models,param=params)
+            model_report:dict=evaluate_models(X_train=X_train,y_train=y_train,X_test=X_test,y_test=y_test,models=models,param=params)
             
             ## To get best model score from dict
             best_model_score = max(sorted(model_report.values()))
@@ -109,10 +107,6 @@ class ModelTrainer:
 
             r2_square = r2_score(y_test, predicted)
             return r2_square
-            
-
-
-
             
         except Exception as e:
             raise CustomException(e,sys)
